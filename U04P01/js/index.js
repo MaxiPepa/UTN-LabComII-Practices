80675d1d9c175afc77505604e0f74fad60c1484c
MaxiPepa <maximopepa@gmail.com>
2022-06-03 12:51:54 -0300

    Practica de la U4 terminada

--- U04P01/js/index.js
+++ U04P01/js/index.js
@@ -31,30 +31,92 @@ function obtainData() {
     var birthday = document.getElementById("birthday").value;
     var province = document.getElementById("province").value;
 
-    var newPerson = new Person(name, age, birthday, province);
-    personList.push(newPerson);
-
-    showData(newPerson);
-
-    document.getElementById("form").reset();
+    validateData(name, age, birthday, province)
 }
 
-function showData(person) {
-    let name = person.getName()
-    let age = person.getAge()
-    let birthday = person.getBirthday()
-    let province = person.getProvince()
+function validateData(name, age, birthday, province) {
+    let resultadoValidacion = true;
+    let errores = []
 
-    let row = ` <tr>
-                    <td>${name}</td>
-                    <td>${age}</td>
-                    <td>${birthday}</td>
-                    <td>${province}</td>
-                </tr>
-                `
-    document.getElementById("personTable").innerHTML += row
+    if (name === "") {
+        resultadoValidacion = false;
+        errores.push("Debe ingresar el nombre")
+    }
+    if (name.length < 8) {
+        resultadoValidacion = false;
+        errores.push("Nombre debe ser mayor a 8 letras")
+    }
+    if (name.length > 50) {
+        resultadoValidacion = false;
+        errores.push("Nombre debe ser menor a 50 letras")
+    }
+    if (age === "") {
+        resultadoValidacion = false;
+        errores.push("Debe ingresar la edad")
+    }
+    if (age < 18) {
+        resultadoValidacion = false;
+        errores.push("Debe ingresar la edad mayor a 18")
+    }
+    if (age > 100) {
+        resultadoValidacion = false;
+        errores.push("Debe ingresar la edad menor a 100")
+    }
+    if (birthday === "") {
+        resultadoValidacion = false;
+        errores.push("Debe ingresar la fecha")
+    }
+    if (province === "") {
+        resultadoValidacion = false;
+        errores.push("Debe ingresar la provincia")
+    }
+
+    if (resultadoValidacion) {
+        var newPerson = new Person(name, age, birthday, province);
+        personList.push(newPerson);
+    
+        document.getElementById("form").reset();
+    } else {
+        alert(errores.join(", "))
+    }
+}
+
+function showData() {
+    let name, age, birthday, province;
+
+    cleanData();
+    
+    for (let i = 0; i < personList.length; i++) {
+        name = personList[i].getName();
+        age = personList[i].getAge();
+        birthday = personList[i].getBirthday();
+        province = personList[i].getProvince();
+
+        let row = ` <tr>
+                        <td>${name}</td>
+                        <td>${age}</td>
+                        <td>${birthday}</td>
+                        <td>${province}</td>
+                    </tr>
+                    `
+
+        document.getElementById("personTable").innerHTML += row;
+    }
+}
+
+function cleanData() {
+    let table = document.getElementById("personTable");
+
+    while(table.children[1]) {
+        table.removeChild(table.children[1]);
+    }
 }
 
 var sendButton = document.getElementById("send");
-
 sendButton.onclick = obtainData;
+
+var showButton = document.getElementById("show");
+showButton.onclick = showData(personList);
+
+var cleanButton = document.getElementById("clean");
+cleanButton.onclick = cleanData;
